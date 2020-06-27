3f86ddb2fc63eb7ad6aa3a5ba1f29fa5ba941b8c
fargusMD <bragaruion@gmail.com>
2020-06-27 22:29:58 +0400

    Updates

---
+++ precache-manifest.26cf69909728629782edd5a3aa98145b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e160c87cf99b92c54243863c05be8359",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "581bfd8d55710a11ece8",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "6bf533b542f82124a0c6",
+    "url": "/portfolio/static/css/main.8715ae28.chunk.css"
+  },
+  {
+    "revision": "581bfd8d55710a11ece8",
+    "url": "/portfolio/static/js/2.ed2e50ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ed2e50ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf533b542f82124a0c6",
+    "url": "/portfolio/static/js/main.83702997.chunk.js"
+  },
+  {
+    "revision": "d4ed5c68e79436c711a8",
+    "url": "/portfolio/static/js/runtime-main.6e0b3c88.js"
+  },
+  {
+    "revision": "54ae0457fa2049d45d5d11e2d28a2a2c",
+    "url": "/portfolio/static/media/casino-api.54ae0457.png"
+  },
+  {
+    "revision": "4c2215ce05b00da372e7fe4c8ae158e3",
+    "url": "/portfolio/static/media/food-travel.4c2215ce.png"
+  },
+  {
+    "revision": "7f29fa4e44ac624528d418ff3c266bda",
+    "url": "/portfolio/static/media/food-travel.7f29fa4e.png"
+  },
+  {
+    "revision": "8ffc6e2d1e0c928991e854e4ba0218d7",
+    "url": "/portfolio/static/media/footer.8ffc6e2d.jpg"
+  },
+  {
+    "revision": "3f57f32ad4352d3d8a2b8386fac36906",
+    "url": "/portfolio/static/media/hero2.3f57f32a.jpg"
+  },
+  {
+    "revision": "6baa81e9cfbbc338f44c5a9409bc90b3",
+    "url": "/portfolio/static/media/ls-blog.6baa81e9.png"
+  },
+  {
+    "revision": "8bce280a3a9829a908cafadf3056ed8c",
+    "url": "/portfolio/static/media/my-beautiful-awesome-face.8bce280a.jpg"
+  },
+  {
+    "revision": "e4c0f49df14c8aff9f0fd651bc933dea",
+    "url": "/portfolio/static/media/s7.e4c0f49d.png"
+  },
+  {
+    "revision": "2c52760f14cc8238ff74d67d96112693",
+    "url": "/portfolio/static/media/travel-agency.2c52760f.jpg"
+  }
+]);
